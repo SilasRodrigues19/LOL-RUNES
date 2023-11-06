1eb488ce7495786650a97fe8f952e52f45a4e7a6
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-11-06 09:56:45 -0300

    refactor: update route using node-fetch2 instead got

--- src/routes/getRunes.ts
+++ src/routes/getRunes.ts
@@ -1,35 +1,59 @@
 import 'dotenv/config';
 
 import { FastifyInstance } from 'fastify';
-import got from 'got';
+import fetch from 'node-fetch';
 
 const apiKey = process.env.RIOT_API_KEY;
-const summonerId = process.env.SUMMONER_ID;
 const region = process.env.REGION;
 
-const runes = require('../../runes.json');
+import runes from '../../runes.json';
 
 export const getRunesRoute = async (app: FastifyInstance) => {
   app.get('/getRunesRoute', async () => {
+
     try {
+
+      const summonerIdResponse = await fetch(
+        'http://localhost:3333/getSummonerId',
+        { method: 'GET' }
+      );
+      const summonerData = await summonerIdResponse.text();
+      const summonerId: string = summonerData;
+      
       const url = `https://${region}.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${summonerId}?api_key=${apiKey}`;
 
-      console.log(url)
+      // console.log(url);
 
-      const response = await got(url);
+      const response = await fetch(url, { method: 'GET' });
 
-      const data: any = JSON.parse(response.body);
+      if (!response.ok) {
+        switch (response.status) {
+          case 400:
+            return `[DEBUG] ${await response.text()}`;
+          case 401:
+          case 403:
+            return 'APIKey inválida ou expirada gere uma nova em https://developer.riotgames.com/';
+          case 404:
+            return 'Use !runas ou !runes após o jogo iniciar';
+          case 429:
+            return 'Aguarde alguns minutos e use o comando novamente';
+          default:
+            throw new Error(`Erro desconhecido: ${response.status}`);
+        }
+      }
+
+      const data = await response.json();
 
       const participant = data.participants.find(
-        (p) => p.summonerId === summonerId
+        (p: {summonerId: string}) => p.summonerId === summonerId
       );
 
       if (!participant) return `${summonerId} não está na partida`;
 
       if (data && data.participants) {
-        participant.perks.perkIds = participant.perks.perkIds.map((perkId) => {
+        participant.perks.perkIds = participant.perks.perkIds.map((perkId: number) => {
           const perkInfo = runes.find((perk) => perk.id === perkId);
-          return perkInfo ? perkInfo.name : `Runa Desconhecida`;
+          return perkInfo ? perkInfo.namePt : `Runa Desconhecida`;
         });
 
         const runesText = `────────────────────────────────
@@ -40,22 +64,8 @@ export const getRunesRoute = async (app: FastifyInstance) => {
 
         return runesText;
       }
-    } catch (error) {
-      
-      switch (error.response.statusCode) {
-        case 400:
-          return `[DEBUG] ${error.response.body}`;
-        case 401:
-        case 403:
-          return 'APIKey inválida ou expirada gere uma nova em https://developer.riotgames.com/'
-        case 404:
-          return 'Use !runas ou !runes após o jogo iniciar';
-        case 429:
-          return 'Aguarde alguns minutos e use o comando novamente';
-        default:
-          throw error;
-      }
-
+    } catch (err) {
+      throw err; 
     }
   });
 };
---
+++ src/routes/index.ts
@@ -0,0 +1,2 @@
+export * from './getRunes';
+export * from './getSummonerId';
